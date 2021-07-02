b09fd3fd06e7ff77aa0de60cd070accd09a118c5
Freddy González <7684330+agonzalezio@users.noreply.github.com>
2021-07-01 23:04:22 -0500

    Change button shadow

--- src/views/Editor.jsx
+++ src/views/Editor.jsx
@@ -206,8 +206,8 @@ const Button = styled.button`
   background: #222f3e;
   border: 0;
   border-radius: 4px;
-  box-shadow: 0 12px 20px -5px rgba(0, 0, 0, 0.1),
-    0 15px 30px -12px rgba(0, 0, 0, 0.05);
+  box-shadow: 0 4px 6px -5px rgba(0, 0, 0, 0.15),
+    0 12px 15px -5px rgba(0, 0, 0, 0.1);
   color: #fff;
   cursor: pointer;
   display: inline-block;
