11e8047ae9ee9f933abece5b832d9c20f66b5275
Freddy González <7684330+agonzalezio@users.noreply.github.com>
2021-08-29 17:35:03 -0500

    Setup Auth0

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import { UserProvider } from '@auth0/nextjs-auth0'
 import 'lib/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0'
 
 const IndexPage: NextPage = () => {
-  return <h1>Hello world</h1>
+  const { user, error, isLoading } = useUser()
+  if (isLoading) return <h1>Loading</h1>
+  if (error) return <h1>Error</h1>
+
+  return (
+    <div>
+      <h1>Hello, {user?.name}</h1>
+      {user ? (
+        <Link href="/create">Create a form</Link>
+      ) : (
+        <a href="/api/auth/login">Log In</a>
+      )}
+    </div>
+  )
 }
 
 export default IndexPage
