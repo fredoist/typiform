cdd17647926bc8432841fa24a70d6cd54904a46c
Freddy González <7684330+agonzalezio@users.noreply.github.com>
2021-08-31 21:16:27 -0500

    Remove flex div from sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -60,176 +60,171 @@ const Sidebar = ({ show }: { show: boolean }) => {
             </a>
           )}
         </div>
-        <div className="flex-1">
-          {!user ? (
-            <div className="p-4">
-              <div className="bg-gray-100 p-4 rounded border border-gray-200">
-                <h2 className="font-semibold text-lg mb-4">Log In</h2>
-                <p className="text-sm">
-                  Create an account and get access to all features
-                </p>
-                <div className="mt-2">
-                  <a
-                    href="/api/auth/login?returnTo=/create"
-                    className="btn border border-gray-300 justify-center gap-4 hover:bg-gray-200"
-                  >
-                    <span>Log In</span>
-                    <LoginIcon className="icon text-gray-500" />
-                  </a>
-                </div>
+        {!user ? (
+          <div className="p-4 flex-1">
+            <div className="bg-gray-100 p-4 rounded border border-gray-200">
+              <h2 className="font-semibold text-lg mb-4">Log In</h2>
+              <p className="text-sm">
+                Create an account and get access to all features
+              </p>
+              <div className="mt-2">
+                <a
+                  href="/api/auth/login?returnTo=/create"
+                  className="btn border border-gray-300 justify-center gap-4 hover:bg-gray-200"
+                >
+                  <span>Log In</span>
+                  <LoginIcon className="icon text-gray-500" />
+                </a>
               </div>
             </div>
-          ) : (
-            <React.Fragment>
-              <div className="p-4">
-                <label
-                  htmlFor="form-filter"
-                  className="block relative bg-white"
-                >
-                  <SearchIcon className="icon absolute left-3 top-3 cursor-text" />
-                  <input
-                    type="search"
-                    name="form-filter"
-                    id="form-filter"
-                    placeholder="Type in to filter your forms"
-                    className="w-full p-2 rounded border bg-transparent pl-8 border-gray-300 focus:ring-4 focus:ring-black/10 focus:border-gray-400 focus:outline-none transition-colors"
-                    onChange={(e) => {
-                      const target = e.target as HTMLInputElement
-                      if (userForms) {
-                        setUserForms(
-                          forms.filter((form: any) =>
-                            form.title
-                              .toLocaleLowerCase()
-                              .includes(target.value.toLocaleLowerCase())
-                          )
+          </div>
+        ) : (
+          <React.Fragment>
+            <div className="p-4">
+              <label htmlFor="form-filter" className="block relative bg-white">
+                <SearchIcon className="icon absolute left-3 top-3 cursor-text" />
+                <input
+                  type="search"
+                  name="form-filter"
+                  id="form-filter"
+                  placeholder="Type in to filter your forms"
+                  className="w-full p-2 rounded border bg-transparent pl-8 border-gray-300 focus:ring-4 focus:ring-black/10 focus:border-gray-400 focus:outline-none transition-colors"
+                  onChange={(e) => {
+                    const target = e.target as HTMLInputElement
+                    if (userForms) {
+                      setUserForms(
+                        forms.filter((form: any) =>
+                          form.title
+                            .toLocaleLowerCase()
+                            .includes(target.value.toLocaleLowerCase())
                         )
-                      }
-                    }}
-                  />
-                </label>
-              </div>
-              <div className="flex-1 overflow-y-auto custom-scrollbar">
-                {userForms &&
-                  userForms.map((form: any) => (
-                    <div
-                      key={form.id}
-                      className="flex items-center hover:bg-gray-200 justify-between"
-                    >
-                      <Link href={`/${form.id}`}>
-                        <a className="py-2 px-4 flex items-center gap-2 truncate w-full">
-                          {form.header.icon ? (
-                            <Image
-                              src={form.header.icon}
-                              alt="Icon"
-                              width={18}
-                              height={18}
-                              unoptimized={true}
-                            />
-                          ) : (
-                            <DocumentTextIcon className="icon text-gray-500" />
-                          )}
-                          <span className="w-full truncate">
-                            {form.title ? form.title : 'Untitled form'}
-                          </span>
-                        </a>
-                      </Link>
-                      <Menu as="div" className="relative">
-                        <Menu.Button className="p-2">
-                          <span className="sr-only">Show form options</span>
-                          <DotsHorizontalIcon className="icon" />
-                        </Menu.Button>
-                        <Transition
-                          as={React.Fragment}
-                          enter="transition duration-200"
-                          enterFrom="scale-95 opacity-0"
-                          enterTo="scale-100 opacity-100"
-                          leave="transition duration-95"
-                          leaveFrom="scale-100 opacity-100"
-                          leaveTo="scale-95 opacity-0"
-                        >
-                          <Menu.Items className="absolute right-0 shadow-lg bg-white ring-1 ring-black/10 rounded z-50 py-2">
-                            <Menu.Item as={React.Fragment}>
-                              {({ active }) => (
-                                <button
-                                  className={cx(
-                                    'py-2 px-4 hover:bg-gray-100 flex items-center gap-2 w-full',
-                                    { 'bg-gray-100': active }
-                                  )}
-                                  onClick={() => {
-                                    const duplicateForm = fetch(`/api/forms`, {
-                                      method: 'POST',
-                                      body: JSON.stringify({
-                                        ...form,
-                                        id: undefined,
-                                      }),
+                      )
+                    }
+                  }}
+                />
+              </label>
+            </div>
+            <div className="flex-1 overflow-y-auto custom-scrollbar">
+              {userForms &&
+                userForms.map((form: any) => (
+                  <div
+                    key={form.id}
+                    className="flex items-center hover:bg-gray-200 justify-between"
+                  >
+                    <Link href={`/${form.id}`}>
+                      <a className="py-2 px-4 flex items-center gap-2 truncate w-full">
+                        {form.header.icon ? (
+                          <Image
+                            src={form.header.icon}
+                            alt="Icon"
+                            width={18}
+                            height={18}
+                            unoptimized={true}
+                          />
+                        ) : (
+                          <DocumentTextIcon className="icon text-gray-500" />
+                        )}
+                        <span className="w-full truncate">
+                          {form.title ? form.title : 'Untitled form'}
+                        </span>
+                      </a>
+                    </Link>
+                    <Menu as="div" className="relative">
+                      <Menu.Button className="p-2">
+                        <span className="sr-only">Show form options</span>
+                        <DotsHorizontalIcon className="icon" />
+                      </Menu.Button>
+                      <Transition
+                        as={React.Fragment}
+                        enter="transition duration-200"
+                        enterFrom="scale-95 opacity-0"
+                        enterTo="scale-100 opacity-100"
+                        leave="transition duration-95"
+                        leaveFrom="scale-100 opacity-100"
+                        leaveTo="scale-95 opacity-0"
+                      >
+                        <Menu.Items className="absolute right-0 shadow-lg bg-white ring-1 ring-black/10 rounded z-50 py-2">
+                          <Menu.Item as={React.Fragment}>
+                            {({ active }) => (
+                              <button
+                                className={cx(
+                                  'py-2 px-4 hover:bg-gray-100 flex items-center gap-2 w-full',
+                                  { 'bg-gray-100': active }
+                                )}
+                                onClick={() => {
+                                  const duplicateForm = fetch(`/api/forms`, {
+                                    method: 'POST',
+                                    body: JSON.stringify({
+                                      ...form,
+                                      id: undefined,
+                                    }),
+                                  })
+                                  toast
+                                    .promise(duplicateForm, {
+                                      loading: `Duplicating form`,
+                                      success: `Redirecting to edit page`,
+                                      error: `Error while duplicating form`,
                                     })
-                                    toast
-                                      .promise(duplicateForm, {
-                                        loading: `Duplicating form`,
-                                        success: `Redirecting to edit page`,
-                                        error: `Error while duplicating form`,
+                                    .then((res) => res.json())
+                                    .then(({ id }) => {
+                                      mutate(
+                                        `/api/forms/user/${user.sub}`
+                                      ).then(() => {
+                                        router.push(`/${id}/edit`)
                                       })
-                                      .then((res) => res.json())
-                                      .then(({ id }) => {
-                                        mutate(
-                                          `/api/forms/user/${user.sub}`
-                                        ).then(() => {
-                                          router.push(`/${id}/edit`)
-                                        })
+                                    })
+                                }}
+                              >
+                                <DuplicateIcon className="icon text-gray-500" />
+                                <span>Duplicate</span>
+                              </button>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item as={React.Fragment}>
+                            {({ active }) => (
+                              <button
+                                className={cx(
+                                  'py-2 px-4 hover:bg-gray-100 flex items-center gap-2 w-full',
+                                  { 'bg-gray-100': active }
+                                )}
+                                onClick={() => {
+                                  const deleteForm = fetch(
+                                    `/api/forms/${form.id}/delete`,
+                                    {
+                                      method: 'DELETE',
+                                      body: JSON.stringify({
+                                        workspace: form.workspace,
+                                      }),
+                                    }
+                                  )
+                                  toast
+                                    .promise(deleteForm, {
+                                      loading: `Deleting form`,
+                                      success: `Form has been deleted`,
+                                      error: `Error while deleting form`,
+                                    })
+                                    .then(() => {
+                                      mutate(
+                                        `/api/forms/user/${user.sub}`
+                                      ).then(() => {
+                                        router.push(`/${forms[0].id}`)
                                       })
-                                  }}
-                                >
-                                  <DuplicateIcon className="icon text-gray-500" />
-                                  <span>Duplicate</span>
-                                </button>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item as={React.Fragment}>
-                              {({ active }) => (
-                                <button
-                                  className={cx(
-                                    'py-2 px-4 hover:bg-gray-100 flex items-center gap-2 w-full',
-                                    { 'bg-gray-100': active }
-                                  )}
-                                  onClick={() => {
-                                    const deleteForm = fetch(
-                                      `/api/forms/${form.id}/delete`,
-                                      {
-                                        method: 'DELETE',
-                                        body: JSON.stringify({
-                                          workspace: form.workspace,
-                                        }),
-                                      }
-                                    )
-                                    toast
-                                      .promise(deleteForm, {
-                                        loading: `Deleting form`,
-                                        success: `Form has been deleted`,
-                                        error: `Error while deleting form`,
-                                      })
-                                      .then(() => {
-                                        mutate(
-                                          `/api/forms/user/${user.sub}`
-                                        ).then(() => {
-                                          router.push(`/${forms[0].id}`)
-                                        })
-                                      })
-                                  }}
-                                >
-                                  <TrashIcon className="icon text-gray-500" />
-                                  <span>Delete</span>
-                                </button>
-                              )}
-                            </Menu.Item>
-                          </Menu.Items>
-                        </Transition>
-                      </Menu>
-                    </div>
-                  ))}
-              </div>
-            </React.Fragment>
-          )}
-        </div>
+                                    })
+                                }}
+                              >
+                                <TrashIcon className="icon text-gray-500" />
+                                <span>Delete</span>
+                              </button>
+                            )}
+                          </Menu.Item>
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
+                ))}
+            </div>
+          </React.Fragment>
+        )}
         <div>
           <Link href="/create">
             <a className="flex items-center gap-2 px-2 py-3 w-full border-t border-gray-300 hover:bg-gray-100 transition-colors">
