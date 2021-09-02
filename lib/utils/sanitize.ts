a759e1de61977a58572744bbc23d26ea364eacec
Freddy González <7684330+agonzalezio@users.noreply.github.com>
2021-09-02 10:50:17 -0500

    Fix: avoid including dots on react-hook-form register method

--- lib/utils/sanitize.ts
+++ lib/utils/sanitize.ts
@@ -6,7 +6,7 @@ function sanitize(string: string) {
     '"': '&quot;',
     "'": '&#x27;',
     '/': '&#x2F;',
-    '.': '\\\\.',
+    '.': '_', // https://github.com/react-hook-form/react-hook-form/issues/676
   }
   const reg = /[&<>"'/\.]/gi
   return string.replace(reg, (match: string) => map[match])
